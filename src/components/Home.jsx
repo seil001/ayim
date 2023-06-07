2780fd40b9822e270c6b378bc9aac2f4b098c019
bekanara <anara.bekbolotova.on@gmail.com>
2023-06-07 20:11:08 +0600

    homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Home.css";
+import kgz from "../assets/kgz.mp4";
 
 const Home = () => {
   return (
-    <>
-      <div className="home"></div>;
-    </>
+    <div className="home">
+      <div className="overlay"></div>
+      <video src={kgz} autoPlay loop muted />
+      <div className="content">
+        <h1>АЙЫМ</h1>
+      </div>
+    </div>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,44 @@
 import React, { useState } from "react";
-import { MenuData } from "./MenuData";
 import "./Navbar.css";
-
+import ay3 from "../assets/ay3.jpeg";
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <div>
-      <nav className="NavbarItems">
-        <h1 className="logo">
-          Ayim <i class="fa-brands fa-slack"></i>
-        </h1>
-        <div className="menu-icons">
-          <i className="fa-solid fa-bars"></i>
-        </div>
-
-        <ul className="nav-menu">
-          {MenuData.map((item, index) => {
-            return (
-              <li key={index}>
-                <a href={item.url} className={item.cName}>
-                  <i className={item.icon}></i>
-                  {item.title}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-    </div>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={ay3} alt="Айым" />
+      </div>
+      <ul className={`navbar-links ${isOpen ? "active" : ""}`}>
+        <li>
+          <a href="#">Главная</a>
+        </li>
+        <li>
+          <a href="#">О нас</a>
+        </li>
+        <li>
+          <a href="#">Специалисты</a>
+        </li>
+        <li>
+          <a href="#">Форум</a>
+        </li>
+        <li>
+          <a href="#">Контакты</a>
+        </li>
+        <li>
+          <a href="#"></a>
+        </li>
+      </ul>
+      <button className="sign-up">Регистрация</button>
+      <button className="login">Авторизация</button>
+      <div className="burger" onClick={toggleMenu}>
+        <div className={`line ${isOpen ? "line-1" : ""}`}></div>
+        <div className={`line ${isOpen ? "line-2" : ""}`}></div>
+        <div className={`line ${isOpen ? "line-3" : ""}`}></div>
+      </div>
+    </nav>
   );
 };
-
 export default Navbar;
